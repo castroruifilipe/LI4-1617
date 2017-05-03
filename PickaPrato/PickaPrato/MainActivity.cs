71ce819373466db6d1f1bdef5173d63eb16a000c
Rui Filipe Castro Leite <ruicastro@outlook.com>
2017-05-03 23:19:29 +0100

    li

---
+++ PickaPrato/PickaPrato/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PickaPrato {
+    
+    [Activity(Label = "Pick'a Prato", MainLauncher = true, Icon = "@mipmap/icon")]
+
+    public class MainActivity : Activity {
+        
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var registarButtom = FindViewById<Button>(Resource.Id.registar);
+            registarButtom.Click += (sender, e) => {
+                StartActivity(typeof(Registar));
+			};
+        }
+    }
+}
+
---
+++ PickaPrato/PickaPrato/Registar.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.Design.Widget;
+
+namespace PickaPrato
+{
+
+    [Activity(Label = "Registar")]
+
+    public class Registar : Activity {
+        
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Registar);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetActionBar(toolbar);
+            ActionBar.Title = "Registar";
+
+            var tablayout = FindViewById<TabLayout>(Resource.Id.tabs);
+			tablayout.AddTab(tablayout.NewTab().SetText("Cliente"));
+            tablayout.AddTab(tablayout.NewTab().SetText("Proprietário"));
+        }
+    }
+}
