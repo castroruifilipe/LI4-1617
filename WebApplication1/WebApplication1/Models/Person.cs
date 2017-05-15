a7240853b90511427419da086b34daaf202ae0b7
Joao Paulo Ribeiro Alves <Joao Paulo Ribeiro Alves>
2017-05-15 17:15:01 +0100

    Rest services template

---
+++ WebApplication1/WebApplication1/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Person
+    {
+        public String Username { get; set; }
+
+        public String Password { get; set; }
+        
+    }
+}
