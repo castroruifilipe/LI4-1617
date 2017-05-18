089bffdc45e230cf3356b4a47f09842e093d0906
Rui Filipe Castro Leite <ruicastro@outlook.com>
2017-05-18 14:03:10 +0100

    atual

---
+++ PickaPrato/PickaPrato/Data/ServiceEngine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PickaPrato.Business;
+
+namespace PickaPrato.Data {
+    
+    public class ServiceEngine {
+
+        private HttpClient client;
+        
+        public ServiceEngine(string url) {
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+        }
+
+        public async Task<Person> GetCliente() {
+            Console.Out.Write("\n\n\n\n\nAqui1\n\n\n\n");
+            var response = client.GetAsync("api/Default/").Result;
+            Console.Out.Write("\n\n\n\n\nAqui2\n\n\n\n");
+            if (response.IsSuccessStatusCode) {
+                Console.Out.Write("\n\n\n\n\nAqui3\n\n\n\n");
+                var stream = await response.Content.ReadAsStreamAsync();
+                Console.Out.Write("\n\n\n\n\nAqui4\n\n\n\n");
+                var sr = new StreamReader(stream);
+                Console.Out.Write("Aqui5");
+                var jsonReader = new JsonTextReader(sr);
+                Console.Out.Write("Aqui6");
+
+                var serializer = new JsonSerializer();
+                Console.Out.Write("Aqui7");
+                Person p = serializer.Deserialize<Person>(jsonReader);
+                Console.Out.Write("Aqui8");
+
+                return p;
+            } else {
+                return null;
+            }
+        }
+    }
+}
--- PickaPrato/PickaPrato/Presentation/RegistarCliente.cs
+++ PickaPrato/PickaPrato/Presentation/RegistarCliente.cs
@@ -17,6 +17,7 @@ using System.Threading.Tasks;
 using PickaPrato.Business;
 using System.Net;
 using System.IO;
+using PickaPrato.Data;
 
 namespace PickaPrato.Presentation {
 
@@ -28,6 +29,7 @@ namespace PickaPrato.Presentation {
 		private ImageView imageView;
         private CardView cardv;
         private SupportToolbar toolbar;
+        private ServiceEngine se = new ServiceEngine("http://10.0.2.2:5001/");
         
         
         protected override void OnCreate(Bundle savedInstanceState) {
@@ -53,46 +55,15 @@ namespace PickaPrato.Presentation {
             EditText pass = FindViewById<EditText>(Resource.Id.password_edittext);
             Console.Out.WriteLine("\n\n\n\n\n\nOlá\n\n\n\n\n\n");
             Button botaoRegistar = FindViewById<Button>(Resource.Id.bregistar);
-            botaoRegistar.Click += async (sender, e) =>
+            botaoRegistar.Click += (sender, e) =>
             {
-                string url = "http://localhost:50646/api/Default/12";
-                Person p = new Person();
-                p.Username = user.Text;
-                p.Password = pass.Text;
-                Console.Out.WriteLine("\n\n\n\n\n\nCriei Pessoa\n\n\n\n\n\n");
-                JsonValue json = await PostClienteAsync(url, p);
+                Person p = se.GetCliente().Result;
+                Console.Out.Write(p.Username);
             };
 
         }
 
-        private async Task<JsonValue> PostClienteAsync(String url, Person person)
-        {
-            Console.Out.WriteLine("\n\n\n\n\n\nEntrei na Função \n\n\n\n\n\n");
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
-            request.ContentType = "application/json";
-            request.Method = "GET";
-            Console.Out.WriteLine("\n\n\n\n\n\n Criei Coisas \n\n\n\n\n\n");
-            var json = JsonConvert.SerializeObject(person);
-            ASCIIEncoding encoding = new ASCIIEncoding();
-            Byte[] bytes = encoding.GetBytes(json);
 
-            Console.Out.WriteLine("\n\n\n\n\n\n Streams \n\n\n\n\n\n");
-            Stream newStream = request.GetRequestStream();
-            Console.Out.WriteLine("\n\n\n\n\n\n Get Request Stream Estoura \n\n\n\n\n\n");
-            newStream.Write(bytes, 0, bytes.Length);
-            //newStream.Close();
-            Console.Out.WriteLine("\n\n\n\n\n\nRespostas\n\n\n\n\n\n");
-            var response = request.GetResponse();
-            var stream = response.GetResponseStream();
-            JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
-            Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
-
-            var sr = new StreamReader(stream);
-            var content = sr.ReadToEnd();
-            Console.Out.WriteLine("\n\n\n\n\n\nRetorna\n\n\n\n\n\n");
-            return jsonDoc;
-
-        }
 
         private void ButtonOnClick(object sender, EventArgs eventArgs) {
 			Intent = new Intent();
