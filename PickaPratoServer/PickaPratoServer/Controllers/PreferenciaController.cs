4856f6b12785359b7edfd05798e8c9e7cdbea5d8
Unknown <a75399@alunos.uminho.pt>
2017-05-22 23:41:54 +0100

    Preferencia
    Add put da preferencia e DAO

---
+++ PickaPratoServer/PickaPratoServer/Controllers/PreferenciaController.cs
@@ -0,0 +1,47 @@
+﻿using PickaPratoServer.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PickaPratoServer.Controllers
+{
+    public class PreferenciaController : ApiController {
+
+        private PreferenciaDAO preferencias = new PreferenciaDAO();
+
+        // GET: api/Preferencia
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Preferencia/5
+        [HttpGet]
+        public List<string> Get(string id)
+        {
+            List<string> r = new List<string>();
+            Debug.WriteLine("AQUIIIII");
+            r = preferencias.Get(id);
+            return r;
+        }
+
+        // POST: api/Preferencia
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Preferencia/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Preferencia/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PickaPratoServer/PickaPratoServer/Data/PreferenciaDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PickaPratoServer.Data
+{
+    public class PreferenciaDAO
+    {
+        SqlConnection connection = new SqlConnection("Server=DIOGO-PC\\SQLEXPRESS; Database=PickPrato; Trusted_Connection=True;");
+
+        public List<String> Get(String id)
+        {
+            List<string> res = new List<string>();
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = @"
+                select * from Preferencias
+                join Ingrediente
+                on Ingrediente.idIngrediente = Preferencias.ingrediente
+                where Preferencias.cliente = @username
+            ";
+            command.Parameters.Add(new SqlParameter("@username", id));
+            var result = command.ExecuteReader();
+            while (result.Read())
+            {
+                res.Add((String)result["designacao"]);
+            }
+            connection.Close();
+            return res;
+        }
+    }
+}
